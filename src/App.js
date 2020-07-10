a1007ffa886007f14a5dbafbdf7ef762369fa831
Juan Carabante <juan.carab@hotmail.com>
2020-07-10 15:02:34 -0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
+import Resumen from './componentes/Resumen';
+import Resultado from './componentes/Resultado';
+import Spinner from './componentes/Spinner';
+import PropTypes from 'prop-types';
+
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div `
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div `
+    background-color: #FFFFFF;
+    padding: 3rem;
+`;
 
 function App() {
+
+    const [ resumen, guardarResumen ] = useState({
+        cotizacion: 0,
+        datos: {
+            marca: '',
+            year: '',
+            plan:''
+        }
+
+    });
+
+    const [ cargando, guardarCargando ] = useState(false);
+
+    // Extraer datos
+    const { cotizacion, datos } = resumen;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+        <Header
+            titulo='Cotizador de seguros'
+            
+        />
+        <ContenedorFormulario>
+            <Formulario 
+                guardarResumen={guardarResumen}
+                guardarCargando={guardarCargando}
+            />
+
+            {cargando ? <Spinner /> : null}
+            
+
+            <Resumen 
+                datos={datos}
+            />
+            {!cargando ?
+                    <Resultado 
+                        cotizacion={cotizacion}
+                    /> : null
+            }
+
+        </ContenedorFormulario>
+    </Contenedor>
   );
+}
+
+Resultado.propTypes = {
+    cotizacion: PropTypes.number.isRequired,
 }
 
 export default App;
